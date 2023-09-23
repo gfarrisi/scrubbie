6624ff782213648b6ac2effc45b5309aedb6229a
Ellie <ellie@bello.lol>
2023-09-23 21:18:16 +0000

    front end

---
+++ components/ScoreIndex.tsx
@@ -0,0 +1,163 @@
+import styles from '@/styles/Home.module.css'
+
+
+const indexColor = {
+  red: '#FF6B6B',
+  orange: '#FF922B',
+  yellow: '#FCC419',
+  green: '#51CF66',
+}
+
+interface IndexBlockProps {
+  range: {
+    min: number,
+    max: number
+  }
+  score: number
+  color: string
+}
+
+function IndexBlock(props: IndexBlockProps) {
+  const { range, score, color } = props
+  const ratio = 20
+  const { min, max } = range
+  const width = (max - min) * ratio;
+
+  const containsScore = score >= min && score <= max;
+
+  console.log({
+    color,
+    containsScore
+  })
+
+  return (
+    <>
+      <div
+        style={{
+            height: 7,
+            width: `${width}%`,
+            backgroundColor: color,
+            borderRadius: `${min === 0 && max !== 100 ? '10px 0px 0px 10px' :max === 100 ? '0px 10px 10px 0px' : '0px 0px 0px 0px'}`, 
+            position: 'relative',
+        }}
+      >
+        <div
+        style={{
+          position: 'absolute',
+          top: 10,
+          left: -10,
+        }}
+        >
+          <p>{min}</p>
+        </div>
+        {max === 100 && (
+          <div
+          style={{
+            position: 'absolute',
+            top: 10,
+            left: max + 15,
+          }}
+          >
+            <p>{max}</p>
+            </div>
+            )
+        }
+
+        {
+          containsScore && (
+            <div
+            style={{
+              position: 'absolute',
+              top: -50,
+              left: `${score-18}%`,
+            }}
+            >
+              <div>
+                <div
+                className={styles.scoreIndexNumber}
+                 style={{
+                  position: 'absolute',
+                  top: 8,
+                  left: score === 100 ? 7 : score < 10 ? 14 : 10,
+                  color: color,
+                }}
+                >{score}</div>
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="37" height="45" viewBox="0 0 37 45" fill="none">
+                    <g opacity="0.3">
+                    <rect x="0.935303" y="0.455688" width="35.9318" height="35.9318" rx="17.9659" fill={color}
+                    />
+                    <path d="M18.9012 44.1974L6.00717 30.9777L31.7953 30.9777L18.9012 44.1974Z" fill={color}/>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+              <div
+               style={{
+                position: 'absolute',
+                top: 45,
+                left: 12,
+              }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                  <circle cx="6.90161" cy="6.80627" r="5.73389" fill={color} stroke="white" stroke-width="2"/>
+                </svg>
+              </div>
+            </div>
+          )
+        }
+      </div>
+     
+    </>
+
+  )
+}
+
+
+interface ScoreIndexProps {
+  score: number
+}
+
+export default function ScoreIndex(porps: ScoreIndexProps) {
+   
+    return (
+      <div 
+      className={
+        styles.scoreIndexContainer
+      }
+      >
+        <IndexBlock
+          range={{
+            min: 0,
+            max: 10
+          }}
+          score={porps.score}
+          color={indexColor.red}
+        />
+         <IndexBlock
+          range={{
+            min: 10,
+            max: 30
+          }}
+          score={porps.score}
+          color={indexColor.orange}
+        />
+         <IndexBlock
+          range={{
+            min: 30,
+            max: 80
+          }}
+          score={porps.score}
+          color={indexColor.yellow}
+        />
+         <IndexBlock
+          range={{
+            min: 80,
+            max: 100
+          }}
+          score={porps.score}
+          color={indexColor.green}
+        />
+      </div>
+    )
+}
+  
